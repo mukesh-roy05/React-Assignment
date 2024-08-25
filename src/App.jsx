ddd90047354757b2bb1e3d0c1717240e379aec2a
Mukesh Roy <mukesh.raj60@gmail.com>
2024-08-25 23:20:53 +0530

    Button Show Hide - enable

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,41 @@ import { useEffect, useState } from "react";
 const api = "https://66ca324159f4350f064ea5a8.mockapi.io/users";
 const App = () => {
   const [data, setData] = useState([]);
+  const [isEnable, setIsEnable] = useState(false);
+
   useEffect(() => {
     fetch(api, { method: "GET" })
       .then((response) => response.json())
       .then((res) => setData(res));
   }, []);
+
+  const handleClick = (e) => {
+    console.log(e.target.value);
+    setIsEnable(!isEnable);
+  };
+
   return (
     <div>
       <h1>Book Author</h1>
-      <ul style={{ listStyle: "none" }}>
-        {data.map((item, index) => {
-          return <li key={index}>{item.name}</li>;
-        })}
-      </ul>
+      <button onClick={handleClick}>{isEnable ? "Hide" : "Show"} </button>
+      {isEnable && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
