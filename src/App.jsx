c4c64cd1bdb70027ec6d274c3ca50a45a5e3be09
Mukesh Roy <mukesh.raj60@gmail.com>
2024-08-25 23:50:39 +0530

    axos-router-loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-const api = "https://66ca324159f4350f064ea5a8.mockapi.io/users";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import DataFetch from "./components/DataFetch";
+import DataLoader from "./components/DataLoader";
 const App = () => {
-  const [data, setData] = useState([]);
-  const [isEnable, setIsEnable] = useState(false);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <DataFetch />,
+      loader: DataLoader,
+    },
+  ]);
 
-  const urlFetch = async () => {
-    try {
-      const fetchData = await axios.get(api);
-      setData(fetchData.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    // fetch(api, { method: "GET" })
-    //   .then((response) => response.json())
-    //   .then((res) => setData(res));
-    urlFetch();
-  }, []);
-
-  const handleClick = (e) => {
-    console.log(e.target.value);
-    setIsEnable(!isEnable);
-  };
-
-  return (
-    <div>
-      <h1>Book Author</h1>
-      <button onClick={handleClick}>{isEnable ? "Hide" : "Show"} </button>
-      {isEnable && (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
